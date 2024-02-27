83ecd4fe3afc8166913a480f9f20d3219f6940cc
MagicBear <mb@bilibili.com>
2024-02-28 06:11:48 +0800

    add resolve result for test_ping

--- nftable_router/test-ping.py
+++ nftable_router/test-ping.py
@@ -6,6 +6,8 @@ import sys
 import argparse
 import subprocess
 from pytput import TputFormatter
+import ipdb
+import socket
 
 tf = TputFormatter()
 
@@ -32,17 +34,23 @@ for file in file_path:
     with open(file, "r", encoding="utf-8") as f:
         config = json.load(f)
 
+db = ipdb.City(config['ipdb_v4'])
+print("\n".join(["%30s: %s" % (tf.format("{k:s,cyan}", k=k), tf.format("{v:s,yellow}", v=v)) for k, v in
+                 db.find_map(socket.gethostbyname(args.ip), "CN").items()]))
+
 for n in range(len(config["proxy"])):
     proxy = list(config["proxy"].values())[n]
     # dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com
     dns_rc = subprocess.run(
-        ["dig", "myip.opendns.com", "+short", "@resolver1.opendns.com", "+retry=1", "+timeout=1", "+dscp=%d" % (n + 1), "+tcp"],
+        ["dig", "myip.opendns.com", "+short", "@resolver1.opendns.com", "+retry=1", "+timeout=1", "+dscp=%d" % (n + 1),
+         "+tcp"],
         capture_output=True)
     print(tf.format("{msg:s,cyan}{line:s,yellow,bold}  Mark: {mark:d,green,bold}  IP: {ip:s,%s,bold}  Route: " % (
         "cyan" if dns_rc.returncode == 0 else "bg_red"
     ),
                     msg="[+] PING by Line : ",
-                    ip=dns_rc.stdout.decode("utf-8").strip().split("\n")[-1] if dns_rc.returncode == 0 else " CONNECTION ERROR ",
+                    ip=dns_rc.stdout.decode("utf-8").strip().split("\n")[
+                        -1] if dns_rc.returncode == 0 else " CONNECTION ERROR ",
                     line=list(config["proxy"].keys())[n], mark=proxy['mark']),
           subprocess.run(["ip", "route", "show", "table", "%d" % proxy['mark']], capture_output=True).stdout.
           decode("utf-8").strip())
